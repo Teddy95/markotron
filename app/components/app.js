ab3244e68fa65e8173e8a93b2403ecf25d377094
Andre <webmaster@andre-sieverding.de>
2019-01-17 13:42:02 +0100

    Update app.js

--- app/components/app.js
+++ app/components/app.js
@@ -1,5 +1,6 @@
 require('marko/node-require').install()
 const template = require('./index.marko')
+// var template = require('marko').load(require.resolve('./index.marko'));
 
 template.renderSync({
 	name:'Markotron'
