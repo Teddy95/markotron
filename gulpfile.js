093d39f76453caa4efa97ab841ee2196a3a09510
Andre <webmaster@andre-sieverding.de>
2019-01-18 19:23:56 +0100

    Update gulpfile.js [asset watcher]

--- gulpfile.js
+++ gulpfile.js
@@ -137,6 +137,7 @@ gulp.task('release', gulp.series('build', 'set-env:production', gulp.parallel('p
  * Add watch task for Sass/Scss, Jsx and Js Files
  */
 gulp.task('watch', done => {
+    gulp.watch('app/assets/**/*', gulp.series('copy-assets'))
     gulp.watch('app/components/**/*', gulp.series('compile-marko'))
 	gulp.watch('app/sass/*.scss', gulp.series('compile-sass'))
 	gulp.watch('app/css/*.css', gulp.series('copy-css'))
